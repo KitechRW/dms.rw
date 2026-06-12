8bfb70580ac151460d0a692027330d7fe2d7791f
Alain Iranzi <alainiranzi00@gmail.com>
2026-06-12 13:23:14 +0200

    still handling validation messages

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,15 +1,21 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
-
+const {
+  successResponse,
+  errorResponse,
+} = require("../utils/apiResponse");
 
 const createUser = async (req, res) => {
   try {
     const { name, email, password, role } = req.body;
    
     if (req.user.role === "operator" && role !== "farmer") {
-      return res.status(403).json({
-        message: "Operators can only create farmers",
-      });
+      return res.status(403).json(
+        errorResponse(
+          "Operators can only create farmers",
+          "FORBIDDEN_ROLE"
+        )
+      );
     }
 
     const existingUser = await User.findOne({
@@ -17,8 +23,13 @@ const createUser = async (req, res) => {
     });
 
     if (existingUser) {
-      return res.status(409).json({ message: "User already exists" });
-    }
+      return res.status(409).json(
+  errorResponse(
+    "User already exists",
+    "USER_ALREADY_EXISTS"
+  )
+);
+}
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
@@ -29,34 +40,49 @@ const createUser = async (req, res) => {
       role,
     });
 
-    return res.status(201).json({
-      message: "User created successfully",
+   return res.status(201).json(
+  successResponse(
+    "User created successfully",
+    "USER_CREATED",
+    {
       user: {
         id: user._id,
         name: user.name,
         email: user.email,
         role: user.role,
       },
-    });
+    }
+  )
+);
   } catch (error) {
-    return res.status(500).json({
-      message: "User creation failed",
-      error: error.message,
-    });
+    return res.status(500).json(
+  errorResponse(
+    "User creation failed",
+    "USER_CREATION-FAILED",
+    error.message
+  )
+);
   }
 };
-
-
 const getUsers = async (req, res) => {
   try {
     const users = await User.find().select("-password");
 
-    return res.status(200).json(users);
-  } catch (error) {
-    return res.status(500).json({
-      message: "Failed to fetch users",
-      error: error.message,
-    });
+    return res.status(200).json(
+      successResponse(
+        "Users fetched successfully",
+        "USERS_FETCHED",
+        { users }
+      )
+    );
+  }
+  catch (error) {
+    return res.status(500).json(
+      errorResponse(
+        "Failed to fetch users",
+        "USERS_FETCH_FAILED",
+      )
+    );
   }
 };
 
@@ -66,15 +92,28 @@ const getUserById = async (req, res) => {
     const user = await User.findById(req.params.id).select("-password");
 
     if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
+      return res.status(404).json(
+      errorResponse(
+        "User not found",
+        "USER_NOT_FOUND"
+      )
+    );
+  }
 
-    return res.status(200).json(user);
+    return res.status(200).json(
+   successResponse(
+        "User fetched successfully",
+        "USER_FETCHED",
+        { user }
+      )
+    );
   } catch (error) {
-    return res.status(500).json({
-      message: "Failed to fetch user",
-      error: error.message,
-    });
+    return res.status(500).json(
+      errorResponse(
+        "Failed to fetch user",
+        "USER_FETCH_FAILED",
+      )
+    );
   }
 };
 
